432657c3c2aed2b53e5bc01d58b9dd902daa2fd8
SebasG <gamboasebas@gmail.com>
2019-11-08 13:27:53 -0600

    update al main

--- Battleship/src/battleship/Battleship.java
+++ Battleship/src/battleship/Battleship.java
@@ -5,6 +5,8 @@
  */
 package battleship;
 
+import Server.Server;
+
 /**
  *
  * @author nacho
@@ -16,6 +18,7 @@ public class Battleship {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Server.instancia();
     }
     
 }
